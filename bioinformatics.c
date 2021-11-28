4d0b90d4b2ea20bf13973d5bf53f6aa28c5120f6
Lukman isah Musa <95165085+Lukmanisahmusa@users.noreply.github.com>
2021-11-28 17:41:10 +0100

    Create bioinformatics.c

---
+++ bioinformatics.c
@@ -0,0 +1,20 @@
+
+int main()
+{
+    /*Programme to print out my details
+    Written by:
+        LUKMAN ISAH MUSA
+        BASUG/UG/BHS/ANT/ANA/18/001
+
+    */
+
+
+
+    printf("BIOINFORMATICS");
+    printf("NAME: LUKMAN ISAH MUSA\n");
+    printf("EMAIL: isahmusalukman@gmail.com\n");
+    printf("SLACK USERNAME: @Lukman Isah Musa\n");
+    printf("GITHUB USERNAME: lukmanisahmusa\n");
+    printf("AREA OF INTEREST: SYSTEM BIOLOGY\n");
+    return 0;
+}
